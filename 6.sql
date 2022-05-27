20dc2d8eff4ead239a8a3406360c3d462803d92c
bambi <75845821+anniezhou19@users.noreply.github.com>
2022-05-27 14:08:13 +0200

    Delete 6.sql

--- 6.sql
+++
@@ -1,2 +0,0 @@
---In 6.sql, write a SQL query that lists the names of songs that are by Post Malone.
-SELECT songs.name FROM songs JOIN artists ON songs.artist_id = artists.id WHERE artists.name = "Post Malone";
